a085a0ed2ff2cebc0bf92fda1e208a4d2c790eef
Pavel Popkov <goil2011@yandex.ru>
2023-12-25 22:49:04 +0300

    seminar1

---
+++ seminar1/script.js
@@ -0,0 +1,10 @@
+// Задача 1
+let degreesCelsius = 15;
+let degreesFahrenheit = (9 / 5) * degreesCelsius + 32;
+console.log(`Градусы Цельсия: ${degreesCelsius}`);
+console.log(`Градусы Фарингейта ${degreesFahrenheit}`);
+// Задача 2
+let name = 'Павел';
+console.log(`Имя: ${name}`);
+let amdin = name;
+console.log(`Имя админа: ${amdin}`);
